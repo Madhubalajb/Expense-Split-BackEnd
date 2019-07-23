ed1254f5bdda379418c3c1a3ecc014df558b17a8
Madhubalajb <jbmadhumng@gmail.com>
2019-07-23 19:51:46 +0530

    Implement checkbox

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const env = String(process.env.NODE_ENV)
 if(env != 'production') {
     require('dotenv').config()
 }
-console.log(String(process.env.NODE_ENV))
 
 const express = require('express')
 const cors = require('cors')
@@ -14,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/expenses', expensesRouter)
 
-app.listen(3001, () => {
-    console.log(`Server running on port 3000`);
+const PORT = process.env.PORT
+app.listen(PORT || 5000, () => {
+    console.log(`Server running on port ${PORT}`);
 })
